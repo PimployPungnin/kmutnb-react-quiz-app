c1f2c3a765eca931c187a97ef8fd868f9a32dbef
PimployPungnin <tangmo00pp@gmail.com>
2023-09-15 14:39:52 +0700

    Update Quiz.js

--- quiz-app/src/component/Quiz.js
+++ quiz-app/src/component/Quiz.js
@@ -1,10 +1,12 @@
 import { useEffect, useState } from "react";
 import QuestionsData from "../data/QuestionsData";
+import { DataContext } from "../App";
 
 const Quiz = ()=>{
     //console.log(QuestionsData);
     const [current, setCurrent] = useState(0)
     const [selectChoice,setSelectChoice] = useState("")
+    const {score,setScore,setAppState} = useContext(DataContext)
 
     useEffect(()=>{
         checkAnswer()
@@ -13,24 +15,22 @@ const Quiz = ()=>{
     const checkAnswer=()=>{
         if(selectChoice!==""){
             if(selectChoice===QuestionsData[current].answer){
-                console.log("ตอบถูกและได้คะแนน")
+                setScore(score+1)
+                nextQuestion()
             }else{
-                console.log("ตอบผิดไม่ได้คะแนน T_T")
+                nextQuestion()
             }
         }
     }
-    return(
-        <div className="quiz">
-            <h1>{QuestionsData[current].question}</h1>
-            <div className="choices">
-                <button onClick={()=>selectChoice("A")}>{QuestionsData[current].A}</button>
-                <button onClick={()=>selectChoice("B")}>{QuestionsData[current].B}</button>
-                <button onClick={()=>selectChoice("C")}>{QuestionsData[current].C}</button>
-                <button onClick={()=>selectChoice("D")}>{QuestionsData[current].D}</button>
-            </div>
-            <p>{`${current+1} / ${QuestionsData.length}`}</p>
-        </div>
-    )
+    
+    const nextQuestion=()=>{
+        setSelectChoice("")
+        if(current===QuestionsData.length-1){
+            setAppState("score")
+        }else{
+            setCurrent(current+1)
+        }
+    }
 }
 
 export default Quiz;
