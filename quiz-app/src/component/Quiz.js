a57e7accb524c47ff96824dc61f7a0baac79541c
PimployPungnin <tangmo00pp@gmail.com>
2023-09-15 14:56:38 +0700

    Update Quiz.js

--- quiz-app/src/component/Quiz.js
+++ quiz-app/src/component/Quiz.js
@@ -31,6 +31,18 @@ const Quiz = ()=>{
             setCurrent(current+1)
         }
     }
+    return(
+        <div className="quiz">
+            <h1>{QuestionsData[current].question}</h1>
+            <div className="choices">
+                <button onClick={()=>setSelectChoice("A")}>{QuestionsData[current].A}</button>
+                <button onClick={()=>setSelectChoice("B")}>{QuestionsData[current].B}</button>
+                <button onClick={()=>setSelectChoice("C")}>{QuestionsData[current].C}</button>
+                <button onClick={()=>setSelectChoice("D")}>{QuestionsData[current].D}</button>
+            </div>
+            <p>{`${current+1} / ${QuestionsData.length}`}</p>
+        </div>
+    )
 }
 
 export default Quiz;
