1a8729fc95f40ac7357d190056ee7815e22f4866
PimployPungnin <tangmo00pp@gmail.com>
2023-09-15 14:13:01 +0700

    Update App.js

--- quiz-app/src/App.js
+++ quiz-app/src/App.js
@@ -3,6 +3,8 @@ import Menu from "./component/Menu";
 import Quiz from "./component/Quiz";
 import Score from "./component/Score";
 
+import './App.css'
+
 export const DataContext = createContext()
 
 function App(){
