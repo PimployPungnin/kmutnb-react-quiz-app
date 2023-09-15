f36dd7417ad546cdebcbdfdb12d95f21a722565e
PimployPungnin <tangmo00pp@gmail.com>
2023-09-15 14:51:36 +0700

    Update Score.js

--- quiz-app/src/component/Score.js
+++ quiz-app/src/component/Score.js
@@ -1,7 +1,20 @@
+import { useContext } from "react";
+import { DataContext } from "../App";
+import QuestionsData from "../data/QuestionsData";
+
 const Score = ()=>{
+    const {score,setAppState,setScore} = useContext(DataContext)
+
+    const restartApp=()=>{
+        setScore(0)
+        setAppState("menu")
+    }
+
     return(
         <div className="score">
-            <h1>Score Component</h1>
+            <h1>สรุปผลคะแนน</h1>
+            <h2>{score} / {QuestionsData.length}</h2>
+            <button onClick={restartApp}>ทำแบบทดสอบอีกครั้ง</button>
         </div>
     )
 }
