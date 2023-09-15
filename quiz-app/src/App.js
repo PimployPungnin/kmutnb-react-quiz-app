5ee67a2169c4099c1606d3a2dbd6c3438643b07e
PimployPungnin <tangmo00pp@gmail.com>
2023-09-15 13:58:29 +0700

    Update App.js

--- quiz-app/src/App.js
+++ quiz-app/src/App.js
@@ -1,17 +1,22 @@
-import { useState } from "react";
+import { createContext, useState } from "react";
 import Menu from "./component/Menu";
 import Quiz from "./component/Quiz";
 import Score from "./component/Score";
 
+export const DataContext = createContext()
+
 function App(){
   const [appState, setAppState] = useState("menu")
   return(
-    <div className="App">
+    <DataContext.Provider value={{appState,setAppState}}>
+      <div className="App">
       <h1>Web Development Quiz</h1>
       {appState === "menu" && <Menu/>}
       {appState === "quiz" && <Quiz/>}
       {appState === "score" && <Score/>}
     </div>
+    </DataContext.Provider>
+    
   );
 }
 
