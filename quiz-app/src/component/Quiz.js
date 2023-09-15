4aded74905f0ad4126e95125bac4d616799e0789
PimployPungnin <tangmo00pp@gmail.com>
2023-09-15 14:25:30 +0700

    Update Quiz.js

--- quiz-app/src/component/Quiz.js
+++ quiz-app/src/component/Quiz.js
@@ -4,15 +4,17 @@ import QuestionsData from "../data/QuestionsData";
 const Quiz = ()=>{
     //console.log(QuestionsData);
     const [current, setCurrent] = useState(0)
+    const [selectChoice,setSelectChoice] = useState("")
     return(
         <div className="quiz">
             <h1>{QuestionsData[current].question}</h1>
             <div className="choices">
-                <button>{QuestionsData[current].A}</button>
-                <button>{QuestionsData[current].B}</button>
-                <button>{QuestionsData[current].C}</button>
-                <button>{QuestionsData[current].D}</button>
+                <button onClick={()=>selectChoice("A")}>{QuestionsData[current].A}</button>
+                <button onClick={()=>selectChoice("B")}>{QuestionsData[current].B}</button>
+                <button onClick={()=>selectChoice("C")}>{QuestionsData[current].C}</button>
+                <button onClick={()=>selectChoice("D")}>{QuestionsData[current].D}</button>
             </div>
+            <p>{`${current+1} / ${QuestionsData.length}`}</p>
         </div>
     )
 }
