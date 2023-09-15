4d0b159ea6e97b9331b52ecf9dc0d9f4a3530eff
PimployPungnin <tangmo00pp@gmail.com>
2023-09-15 14:19:15 +0700

    Update Quiz.js

--- quiz-app/src/component/Quiz.js
+++ quiz-app/src/component/Quiz.js
@@ -1,7 +1,18 @@
+import { useState } from "react";
+import QuestionsData from "../data/QuestionsData";
+
 const Quiz = ()=>{
+    //console.log(QuestionsData);
+    const [current, setCurrent] = useState(0)
     return(
         <div className="quiz">
-            <h1>Quiz Component</h1>
+            <h1>{QuestionsData[current].question}</h1>
+            <div className="choices">
+                <button>{QuestionsData[current].A}</button>
+                <button>{QuestionsData[current].B}</button>
+                <button>{QuestionsData[current].C}</button>
+                <button>{QuestionsData[current].D}</button>
+            </div>
         </div>
     )
 }
